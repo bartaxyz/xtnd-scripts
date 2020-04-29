6048c9627d7bcb6e104d72166d1776c3e9a2b356
Ondřej Bárta <email@ondrejbarta.xyz>
2020-04-29 15:41:34 +0200

    Fix build

--- src/carousel.ts
+++ src/carousel.ts
@@ -57,11 +57,14 @@
     }
 
     initEventListeners() {
-      [].forEach.call(this.controlsHandles, (handle: HTMLElement, index) => {
-        handle.addEventListener("click", () => {
-          this.setIndex(index);
-        });
-      });
+      [].forEach.call(
+        this.controlsHandles,
+        (handle: HTMLElement, index: number) => {
+          handle.addEventListener("click", () => {
+            this.setIndex(index);
+          });
+        }
+      );
     }
 
     increaseIndex() {
@@ -133,8 +136,8 @@
     const arrowRightWhiteElements = document.querySelectorAll(
       "[data-icon=arrow-right-white]"
     );
-    [].forEach.call(arrowRightWhiteElements, (element) => {
-      (element as HTMLDivElement).innerHTML =
+    [].forEach.call(arrowRightWhiteElements, (element: HTMLDivElement) => {
+      element.innerHTML =
         '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="16" viewBox="0 0 20 16"><path fill="currentColor" fill-rule="nonzero" d="M1.083 9.51l12.86-.006-4.052 4.061a1.054 1.054 0 0 0-.308.749c0 .282.11.549.308.747l.63.631c.198.198.463.308.744.308.282 0 .547-.109.745-.307l6.946-6.946c.2-.199.308-.464.307-.746.001-.284-.108-.549-.307-.748L12.01.307A1.046 1.046 0 0 0 11.265 0c-.281 0-.546.11-.744.307l-.63.631a1.043 1.043 0 0 0-.308.744c0 .282.11.532.308.73l4.098 4.084H1.069c-.581 0-1.069.5-1.069 1.08v.893c0 .58.502 1.042 1.083 1.042z" /></svg>';
     });
   });
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import "./carousel";
+import "./introVideo";
+import "./logoAnimation";
