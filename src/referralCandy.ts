75a80e31dbad7083ff56beee806b2df02c7f1b28
Ondřej Bárta <email@ondrejbarta.xyz>
2020-08-11 00:55:19 +0200

    Update referralCandy.ts

--- src/referralCandy.ts
+++ src/referralCandy.ts
@@ -43,7 +43,7 @@ const initReferralCandy = () => {
       ></div>
     `;
 
-    const generatedScript = `<script>(function(e){var t,n,r,i,s,o,u,a,f,l,c,h,p,d,v;z="script";l="refcandy-purchase-js";c="refcandy-mint";p="go.referralcandy.com/purchase/";t="data-app-id";r={email:"a",fname:"b",lname:"c",amount:"d",currency:"e","accepts-marketing":"f",timestamp:"g","referral-code":"h",locale:"i","external-reference-id":"k",signature:"ab"};i=e.getElementsByTagName(z)[0];s=function(e,t){if(t){return""+e+"="+encodeURIComponent(t)}else{return""}};d=function(e){return""+p+h.getAttribute(t)+".js?aa=75&"};if(!e.getElementById(l)){h=e.getElementById(c);if(h){o=e.createElement(z);o.id=l;a=function(){var e;e=[];for(n in r){u=r[n];v=h.getAttribute("data-"+n);e.push(s(u,v))}return e}();o.src="//"+d(h.getAttribute(t))+a.join("&");return i.parentNode.insertBefore(o,i)}}})(document);</script>`;
+    const generatedScript = `(function(e){var t,n,r,i,s,o,u,a,f,l,c,h,p,d,v;z="script";l="refcandy-purchase-js";c="refcandy-mint";p="go.referralcandy.com/purchase/";t="data-app-id";r={email:"a",fname:"b",lname:"c",amount:"d",currency:"e","accepts-marketing":"f",timestamp:"g","referral-code":"h",locale:"i","external-reference-id":"k",signature:"ab"};i=e.getElementsByTagName(z)[0];s=function(e,t){if(t){return""+e+"="+encodeURIComponent(t)}else{return""}};d=function(e){return""+p+h.getAttribute(t)+".js?aa=75&"};if(!e.getElementById(l)){h=e.getElementById(c);if(h){o=e.createElement(z);o.id=l;a=function(){var e;e=[];for(n in r){u=r[n];v=h.getAttribute("data-"+n);e.push(s(u,v))}return e}();o.src="//"+d(h.getAttribute(t))+a.join("&");return i.parentNode.insertBefore(o,i)}}})(document);`;
 
     console.log("Referral Candy:", generatedCode, generatedScript);
 
@@ -51,10 +51,9 @@ const initReferralCandy = () => {
     wrapperDiv.innerHTML = generatedCode;
     document.body.appendChild(wrapperDiv);
 
+    // To make sure this code is executed correctly
     setTimeout(() => {
-      const wrapperDivForScript = document.createElement("div");
-      wrapperDivForScript.innerHTML = generatedScript;
-      document.body.appendChild(wrapperDivForScript);
+      eval(generatedScript);
     }, 200);
   };
 
